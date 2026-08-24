551e2a5ddfaa55331dece7ada5465fb2a6f81e31
efraim_coehn <7165241@gmail.com>
2026-08-24 16:21:54 +0300

    Add add_student function

--- manager.py
+++ manager.py
@@ -14,13 +14,21 @@ def save_students(filename, students):
             writer = csv.DictWriter(w, fieldnames=keys)
             writer.writeheader()
             writer.writerows(students)
+# my_students = load_students("students.csv")
+# save_students("students_test.csv", my_students)
+
+def add_student(students, name, grade, class_name):
+    addi_student={name:"name",grade:"grade",class_name:"class_name"}
+    students.append(addi_student)
+# טעינת התלמידים הקיימים
 my_students = load_students("students.csv")
-save_students("students_test.csv", my_students)
 
+# הדפסת כמות התלמידים לפני ההוספה
+print("Before:", len(my_students))
 
+# קריאה לפונקציה להוספת תלמיד חדש
+add_student(my_students, "דוד לוי", "90", "ג1")
 
-#     for i in students:
-#          filename[i]+=students
-#          return filename
-# b=save_students("filename")
-# print(b)
+# הדפסת כמות התלמידים אחרי ההוספה (המספר אמור לגדול ב-1)
+print("After:", len(my_students))
+
