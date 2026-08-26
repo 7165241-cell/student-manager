41bdf3487a3ff8422b579894ea639f952ec970a6
efraim_coehn <7165241@gmail.com>
2026-08-26 10:18:32 +0300

    the lest ptogect

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Student Manager
+We built a list of students, grades, and class in the students.csv file.
+
+In the load_students function, we took students.csv and turned it into a dictionary that prints for each student the name, grade, and class in the dictionary.
+
+In the add_student function, we added a new student dictionary to the list that increases by 1.
+The find_student function returns a student dictionary by name, if not, it returns NONE.
+class average function We selected a specific class and returned the class average
+
+In the top_student function, we built a loop that iterates through the student with the highest score and returns the
+
+In the print_all function, we created a function that prints the names of all students.
--- manager.py
+++ manager.py
@@ -1,12 +1,13 @@
 import csv
 import os
+###1
 def load_students(filename):
         with open (filename,"r")as f:
             n=csv.DictReader(f)  
             return(list(n))
 # a=load_students("students.csv")                 
 # print(a)
-
+###2
 def save_students(filename, students):
     if len(students) > 0:
         keys = students[0].keys()
@@ -16,19 +17,87 @@ def save_students(filename, students):
             writer.writerows(students)
 # my_students = load_students("students.csv")
 # save_students("students_test.csv", my_students)
-
+###3
 def add_student(students, name, grade, class_name):
-    addi_student={name:"name",grade:"grade",class_name:"class_name"}
+    addi_student={"name":name,"grade":grade,"class_name":class_name}
     students.append(addi_student)
-# טעינת התלמידים הקיימים
-my_students = load_students("students.csv")
+###4
+def find_student(students, name):
+    for row in students:
+        if row["name"]==name:
+            return row
+        else:
+           return None
+###5
+def class_average(students, class_name):
+    total=0
+    sumi=0
+    sum_class=0
+    for i in students:
+        if students["class"]==class_name:
+            total+=1
+            sumi+=int (i["grade"])
+            sum_class=sumi//total
+            return sum_class
+        else:
+            return 0
 
-# הדפסת כמות התלמידים לפני ההוספה
-print("Before:", len(my_students))
+###6
+def top_student(students):
+    sumi=0
+    for i in students:
+        if students["grade"]>sumi:
+            sumi=int (i["grade"])
+        if sumi ==0:
+            return None
+        return students["name"]
 
-# קריאה לפונקציה להוספת תלמיד חדש
-add_student(my_students, "דוד לוי", "90", "ג1")
+###7
+def print_all(students):
+    for i in students:
+        print (i["name"])
 
-# הדפסת כמות התלמידים אחרי ההוספה (המספר אמור לגדול ב-1)
-print("After:", len(my_students))
+
+def main():
+    file_name =r"C:\Users\נעמה כהן\Desktop\python_projects\efraim-python\Working with files\students.csv"
+    students=load_students(file_name)
+    while True:
+        print("1 :Show all")
+        print("2: Add student")
+        print("3: Search student")
+        print("4: Class average")
+        print("5: Top student")
+        print("6: Save and exit")
+
+        choice=input("Enter a choice (1-6):")
+        if choice=="1":
+            print_all(students)
+        elif choice == "2":
+            add_student(students)
+            save_students(students)
+        elif choice == "3":
+            find_student(students)
+        elif choice == "4":
+            class_average(students)
+            save_students(students)
+        elif choice == "5":
+            top_student(students)
+            save_students(students)
+        elif choice == "6":
+            print_all(students)
+            save_students(students)
+        else:
+            if choice >"6" or choice < "0":
+                break
+        
+# if __name__ =="__main__":
+#      main()  
+
+main()
+        
+
+
+
+
+
 
